c625c34aa6805964135048a74a498cce091b738d
Emanuel Huffman <ehvdb@mst.edu>
2022-08-02 18:23:51 -0500

    again3

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const cors = require("cors");
@@ -20,10 +20,12 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 //Serve Frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(process.cwd(), "../client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(process.cwd(), "../client/build/index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "../", "client", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
@@ -31,6 +33,6 @@ if (process.env.NODE_ENV === "production") {
 
 app.use(errorHandler);
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
