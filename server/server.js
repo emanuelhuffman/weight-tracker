32569062fdb4ebe4ec2d2e17828043ac2fdb8d4d
Emanuel Huffman <ehvdb@mst.edu>
2022-08-02 18:22:12 -0500

    again2

--- server/server.js
+++ server/server.js
@@ -20,12 +20,10 @@ app.use("/api/users", require("./routes/userRoutes"));
 
 //Serve Frontend
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
+  app.use(express.static(path.resolve(process.cwd(), "../client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "client", "build", "index.html")
-    )
+    res.sendFile(path.resolve(process.cwd(), "../client/build/index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
